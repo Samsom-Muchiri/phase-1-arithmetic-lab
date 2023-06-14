c7b1725f26f06e1f405580e75124a1915e3382e7
Sam .M <you@example.com>
2023-06-14 18:22:15 +0300

    complet

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+const random =parseInt( (Math.random() * 10) + 1)
+
+let num3 = 14;
+let num4 = 5;
+
+let mod = num3 % num4;
+
+const max = (1, 2, 3, 4, 5, 6, 7, 8, 9, 20)
+Math.max(max)
